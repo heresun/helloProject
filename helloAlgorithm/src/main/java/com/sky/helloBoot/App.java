3154002ff6218863b647346dc9153400ca6427c1
sundehui <heresun@foxmail.com>
2024-09-06 15:28:44 +0800

    第一次提交

--- helloAlgorithm/src/main/java/com/sky/helloBoot/App.java
+++ helloAlgorithm/src/main/java/com/sky/helloBoot/App.java
@@ -1,13 +1,16 @@
 package com.sky.helloBoot;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder("d");
+        ServerSocket serverSocket = new ServerSocket(9999);
+
+        System.out.println("Hello World!");
     }
 }
